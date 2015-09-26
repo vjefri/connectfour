82c1b7dcc23477701cb3788c9e34eb382a0c2cd2
vjefri <vjefri@gmail.com>
2015-09-25 21:46:59 -0700

    Made a lot of UI changes to the game. Has a nice gradient. Added selection highlighting to columns. Changed background to a gradient.

--- js/game.js
+++ js/game.js
@@ -7,35 +7,51 @@ var connectFour = {
         var $grid = $('.grid');
 
         // create rows
-        for (var x = 0; x < rows; x++) {
-            $grid.append('<div class="row grid-row x' + x + '"></div>');
+        for (var x = 1; x <= rows; x++) {
+            $grid.append('<div class="row table-row row' + x + '"></div>');
 
             // create columns
-            for (var y = 0; y < columns; y++) {
-                $(".x" + x).append('<div class="cell"' + 'id=' + y + "></div>");
+            for (var y = 1; y <= columns; y++) {
+                $(".row" + x).append('<div class="cell column' + y + '"></div>');
             }
         }
     },
-    // if player clicks on column, get the column id fill the color yellow or red
-    
+
     columnClicked: function() {
-        var id;
-        $(".grid").click(function(event) {
-            id = event.target.id;
-            alert(id);
+        // if player clicks on column, get the column id
+        $(".cell").click(function(event) {
+            var selectedColumn = $(this).attr("class").split(' ');
+            console.log(selectedColumn[1]);
+
+            // when column clicked, change the color of the first cell that is white
+
         });
     },
 
-    // when column clicked, change the color of the first cell that is white
-    // check first row in that column that is white
+    highlightSelectedColumn: function() {
+        $('.cell').hover(function() {
+            // change background color
+            var selectedColumn = $(this).attr("class").split(' ');
+            selectedColumn = "." + selectedColumn[1];
+            console.log(selectedColumn);
+            $(selectedColumn).css({'background-color':'#FFF4C1', 'opacity': '0.5'});
+        }, function() {
+            var selectedColumn = $(this).attr("class").split(' ');
+            selectedColumn = "." + selectedColumn[1];
+            // on mouseout, reset the background colour
+            $(selectedColumn).css({'background-color':'', 'opacity': ''});
+        });
+    },
+
     init: function() {
-        connectFour.createGrid();
-        connectFour.columnClicked();
+        this.createGrid();
+        this.columnClicked();
+        this.highlightSelectedColumn();
     }
 };
 
-//Game starts on Document Ready
-$(document).ready(function() {
-    //Play the game
+// game starts when DOM is Ready
+$(function() {
+    // calls initiation function of game
     connectFour.init();
 });
